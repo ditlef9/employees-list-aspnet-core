76a46af8dcf6aaf08fd69753c130b29ab0ae0dbf
Sindre Ditlefsen <sindre@cdcadvania.com>
2025-01-04 18:34:48 +0100

    Add persons

--- Controllers/PersonsController.cs
+++ Controllers/PersonsController.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+using ServiceContracts.DTO;
 
 namespace CRUDExample.Controllers
 {
   public class PersonsController : Controller
   {
+    // Private fields
+    private readonly IPersonsService _personService;
+
+    // Contructor
+    public PersonsController(IPersonsService personsService){
+      _personService = personsService;
+    }
+
     [Route("persons/index")]
     public IActionResult Index()
     {
-      return View();
+      List <PersonResponse> persons = _personService.GetAllPersons();
+
+      return View(persons); // Views/Persons/Index.cshtml
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using ServiceContracts;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Controllers and views
 builder.Services.AddControllersWithViews();
+
+// Add service into IoC Container
+builder.Services.AddSingleton<ICountriesService, CountriesService>();
+builder.Services.AddSingleton<IPersonsService, PersonsService>();
 
 // App
 var app = builder.Build();
